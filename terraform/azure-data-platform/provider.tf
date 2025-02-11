92a67a9a1eb7809ffd784c258718625e7e3edcac
dreger1997 <davidreger@live.de>
2025-02-11 11:06:49 +0100

    replaced variables with placeholder values for better understanding

--- terraform/azure-data-platform/provider.tf
+++ terraform/azure-data-platform/provider.tf
@@ -6,10 +6,10 @@ terraform {
     }
   }
   backend "azurerm" {
-    resource_group_name  = "ldeazureproject-terraform"
-    storage_account_name = "ldeazureprojecttfstate"
-    container_name       = "tfstate"
-    key                  = "state.tfstate"
+    resource_group_name  = "<your-resource-group-name>"
+    storage_account_name = "<your-storage-account-name>"
+    container_name       = "<your-container-name>"
+    key                  = "<your-key>.state"
   }
 }
 
